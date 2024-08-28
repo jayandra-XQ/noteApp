d6cd75a095dba2f963786cb6aec238e1fee69770
Jayandra Hamal <salonhamal278@gmail.com>
2024-08-28 21:46:16 +0545

    MongoDB Atlas + Mongoose setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,24 @@
+import 'dotenv/config';
+import mongoose from 'mongoose';
 import express from 'express';
 
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-app.get('/', (req,res) => {
-  res.send("hello ,world");
+
+
+// Safely checking the MongoDB connection string
+const mongoConnectionString = process.env.MONGO_CONNECTION_STRING as string;
+
+mongoose.connect(mongoConnectionString)
+.then(()=> {
+  console.log('Connected to MongoDB');
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 })
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+.catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+  process.exit(1);
+})
