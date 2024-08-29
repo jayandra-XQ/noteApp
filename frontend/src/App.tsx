d3667ccfa38ee3f56e628b8d5b2d463c58df26f5
Jayandra Hamal <salonhamal278@gmail.com>
2024-08-29 22:49:55 +0545

    React setup with TypeScript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1 className="text-center mt-4">welcome to bootstrap with react</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
