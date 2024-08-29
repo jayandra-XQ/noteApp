f8e29928eb64f27e18f5a471d58413a17745debf
Jayandra Hamal <salonhamal278@gmail.com>
2024-08-29 21:01:25 +0545

    Update + delete notes (backend)

--- backend/src/controllers/note.controller.ts
+++ backend/src/controllers/note.controller.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from "express";
 import noteModel from "../models/note.model";
+import mongoose from "mongoose";
 
 
 export const getNotes: RequestHandler = async (req,res) => {
@@ -42,6 +43,80 @@ export const createNotes: RequestHandler = async (req,res) => {
   } catch (error) {
     const err = error as Error
     console.log("error in create notes controller",err.message);
+    res.status(500).json({success: false, message: 'Server Error'});
+  }
+};
+
+interface updateNoteParams {
+  noteId: string,
+}
+
+interface updateNoteBody{
+  title?: string,
+  text?: string,
+
+}
+
+
+export const updateNotes: RequestHandler<updateNoteParams, unknown, updateNoteBody, unknown> = async (req,res) => {
+  const noteId = req.params.noteId;
+  const newTitle = req.body.title;
+  const newText = req.body.text;
+  try {
+
+    if (!mongoose.isValidObjectId(noteId)) {
+      return res.status(500).json({success: false, message:"Invalid credientials"})
+    }
+
+    if(!newTitle) {
+      return res.status(400).json({success: false, message:"Title is required"})
+    }
+
+    if(!newText){
+      return res.status(400).json({success: false, message:"Text is required"})
+    }
+
+    const note = await noteModel.findById(noteId).exec();
+    if(!note){
+      return res.status(404).json({success: false, message:"Note not found"})
+    }
+
+    note.title = newTitle;
+    note.text = newText;
+
+    const updatedNote = await note.save();
+    res.status(200).json({success:true, updatedNote})
+
+  } catch (error) {
+    const err = error as Error
+    console.log("error in update notes controller",err.message);
+    res.status(500).json({success: false, message: 'Server Error'});
+  }
+}
+
+
+export const deleteNotes : RequestHandler = async(req,res) => {
+  const noteId = req.params.noteId;
+  try {
+     // Validate ObjectId
+     if (!mongoose.isValidObjectId(noteId)) {
+      return res.status(400).json({ success: false, message: "Invalid note ID" });
+    }
+
+    const deleteNote = await noteModel.findByIdAndDelete(noteId).exec();
+
+     // Check if the note was found and deleted
+     if (!deleteNote) {
+      return res.status(404).json({ success: false, message: "Note not found" });
+    }
+
+    // Return success response
+    return res.status(200).json({ success: true, message: "Note deleted successfully" });
+
+    
+  } catch (error) {
+    const err = error as Error;
+    console.log("error in delete notes controller",err.message);
     res.status(500).json({success: false, message: 'Server Error'});
   }
 }
--- backend/src/routes/note.route.ts
+++ backend/src/routes/note.route.ts
@@ -1,10 +1,17 @@
 import express from 'express';
-import { createNotes, getNotes, getNote } from '../controllers/note.controller';
+import { createNotes, getNotes, getNote, updateNotes, deleteNotes } from '../controllers/note.controller';
 
 const router = express.Router();
 
 router.get('/get-notes', getNotes);
 router.get('/get-note/:noteId', getNote)
+
 router.post('/create-note', createNotes)
+
+router.patch('/update-note/:noteId', updateNotes)
+
+
+router.delete('/delete-note/:noteId', deleteNotes)
+
 
 export default router
